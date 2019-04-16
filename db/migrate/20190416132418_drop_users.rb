e9f2147d0320e05d2af36c915ce5a03dd74b6633
katsulau <efthnyb.1994@gmail.com>
2019-04-16 22:30:15 +0900

    Delete users model

---
+++ db/migrate/20190416132418_drop_users.rb
@@ -0,0 +1,8 @@
+class DropUsers < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :users
+  end
+  def down
+    fail ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_090629) do
+ActiveRecord::Schema.define(version: 2019_04_16_132418) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 2019_04_10_090629) do
     t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
